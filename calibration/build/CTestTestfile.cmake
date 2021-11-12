24fa422d0273b3f24b7be0b1b04df800536d0342
Dongha Chung <haetae.dongha@gmail.com>
2021-11-12 20:52:32 +0900

    delete build

--- calibration/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dongha/dev/bundle_adjustment_test/calibration
-# Build directory: /home/dongha/dev/bundle_adjustment_test/calibration/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
